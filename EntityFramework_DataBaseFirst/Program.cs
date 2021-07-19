d7d271fd3885c7290e720468eddee9d4c1709a08
Cecilia-Vazquez <cecilia-viridiana@outlook.es>
2021-07-19 15:56:10 -0500

    Modificacion y eliminación

--- EntityFramework_DataBaseFirst/Program.cs
+++ EntityFramework_DataBaseFirst/Program.cs
@@ -13,7 +13,7 @@ namespace EntityFramework_DataBaseFirst
             using (PersonasEntities dbContext = new PersonasEntities())
 
             {
-                //var personas = dbContext.Personas.Where( p => p.nombre =="Viridiana").ToList();
+                //var personas = dbContext.Personas.Where( p => p.nombre =="Viridiana").ToList();   Consulta
                 //List<Personas> personas = dbContext.Personas.Where(p => p.edad >= 25 && p.edad <= 30).ToList();
 
                 /* foreach(Personas persona in personas)
@@ -23,7 +23,37 @@ namespace EntityFramework_DataBaseFirst
                     
                 }*/
 
-                Personas persona = new Personas();
+
+
+                /* Personas persona = new Personas();    Agreagar registro
+                persona.nombre = "Viry";
+                persona.apellido = "Flores";
+                persona.edad = 34;
+                persona.idGenero = 1;
+
+                dbContext.Personas.Add(persona);*/
+
+                var persona = dbContext.Personas.Where(p => p.id == 10).SingleOrDefault();
+
+                //persona.apellido = "Ortiz";  Modificacion
+                //dbContext.Entry(persona).State = System.Data.Entity.EntityState.Modified;
+
+                dbContext.Personas.Remove(persona);
+
+
+
+
+                try
+                {
+                    dbContext.SaveChanges();
+                    Console.WriteLine("Persona eliminada");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+
 
                 Console.ReadKey();
             }
