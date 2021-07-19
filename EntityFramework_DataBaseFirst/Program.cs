3c97700786a0eedc60de84be25f98f22c1b07276
Cecilia-Vazquez <cecilia-viridiana@outlook.es>
2021-07-19 15:04:01 -0500

    Agregar archivos de proyecto.

---
+++ EntityFramework_DataBaseFirst/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_DataBaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PersonasEntities dbContext = new PersonasEntities())
+
+            {
+                //var personas = dbContext.Personas.Where( p => p.nombre =="Viridiana").ToList();
+                //List<Personas> personas = dbContext.Personas.Where(p => p.edad >= 25 && p.edad <= 30).ToList();
+
+                /* foreach(Personas persona in personas)
+                {
+                    Console.WriteLine(persona.nombre + persona.apellido);
+                    Console.WriteLine(persona.apellido);
+                    
+                }*/
+
+                Personas persona = new Personas();
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
